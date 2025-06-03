610015fe2c1692e3fe6739d922c17bd241f58eea
Ashutosh Joshi <ashutoshjoshi1995@hotmail.com>
2025-06-03 21:43:01 +0530

    Design of Object Page

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -6,9 +6,21 @@ sap.ui.define([
     return Controller.extend("com.nagarro.www.presalestracker.controller.Detail", {
         onInit() {
             this.oRouter = this.getOwnerComponent().getRouter();
-            this.oModel = this.getOwnerComponent().getModel();
+            this.oModel = this.getOwnerComponent().getModel('comp');
 
             this.oRouter.getRoute("Detail").attachPatternMatched(this._onOppMatched, this);
+            const oExitButton = this.getView().byId("idExitFullScreenOverflowToolbarButton"),
+                oEnterButton = this.getView().byId("idEnterFullScreenOverflowToolbarButton");
+            [oExitButton, oEnterButton].forEach(function (oButton) {
+                oButton.addEventDelegate({
+                    onAfterRendering: function () {
+                        if (this.bFocusFullScreenButton) {
+                            this.bFocusFullScreenButton = false;
+                            oButton.focus();
+                        }
+                    }.bind(this)
+                });
+            }, this);
         },
         toggleAreaPriority(oEvent) {
             debugger;
@@ -18,6 +30,20 @@ sap.ui.define([
             this.getView().bindElement({
                 path: "/ZCDS_PS_MASTER('" + this._id + "')"
             });
+        },
+        onOverflowToolbarButtonFullScreenPress: function () {
+            this.bFocusFullScreenButton = true;
+            let sNextLayout = this.oModel.getProperty("/actionButtonsInfo/midColumn/fullScreen");
+            this.oRouter.navTo("Detail", { layout: sNextLayout, id: this._service });
+        },
+        onOverflowToolbarButtonExitFullScreenPress: function () {
+            this.bFocusFullScreenButton = true;
+            let sNextLayout = this.oModel.getProperty("/actionButtonsInfo/midColumn/exitFullScreen");
+            this.oRouter.navTo("Detail", { layout: sNextLayout, id: this._service });
+        },
+        onOverflowToolbarButtonClosePress: function () {
+            let sNextLayout = this.oModel.getProperty("/actionButtonsInfo/midColumn/closeColumn");
+            this.oRouter.navTo("Master", { layout: sNextLayout });
         }
     });
 });
